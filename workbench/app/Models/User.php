c26cbd09cc1ab36548d8788593e9035e6537da86
Nabil Hassen <nabil.hassen08@gmail.com>
2024-06-05 15:34:23 +0300

    wip

--- workbench/app/Models/User.php
+++ workbench/app/Models/User.php
@@ -5,7 +5,7 @@ namespace Workbench\App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as AuthUser;
 use Nabilhassen\LaravelUsageLimiter\Traits\HasLimits;
-use Orchestra\Testbench\Factories\UserFactory;
+use Workbench\Database\Factories\UserFactory;
 
 class User extends AuthUser
 {
