54ef8ead4772fc19249d7e427973e8c2ff8a2dc5
Nabil Hassen <nabil.hassen08@gmail.com>
2024-06-07 21:01:55 +0300

    wip

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -16,25 +16,11 @@ class ServiceProvider extends SupportServiceProvider
 
     public function boot(): void
     {
-        $this->publishMigration();
-
         $this->publishes([
             __DIR__ . '/../config/limit.php' => config_path('limit.php'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
         ]);
 
         $this->app->bind(Limit::class, $this->app->config['limit.models.limit']);
-    }
-
-    protected function publishMigration(): void
-    {
-        if ($this->app->version() >= 11) {
-            $this->publishesMigrations([
-                __DIR__ . '/../database/migrations' => database_path('migrations'),
-            ]);
-        }
-
-        if ($this->app->version() < 11) {
-            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        }
     }
 }
